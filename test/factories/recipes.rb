04aa402e575700bfd05e58f5f3607c66c0957e3e
irfan87 <nerve2009@yahoo.com>
2018-03-08 23:38:34 +0800

    add recipes controller

--- test/factories/recipes.rb
+++ test/factories/recipes.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :recipe do
-    name "Nasi Air Dingin"
-    description "Nasi Air Dingin is a recipe which from Kampung Dingin"
+    name { Faker::Hipster.sentence }
+    description { Faker::Hipster.paragraph }
+    
     association(:user)
   end
 end
